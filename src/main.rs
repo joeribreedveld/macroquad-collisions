4c0d6bcf79029748e3d625ebf8ca2f36d239d4de
joeribreedveld <joeri.breedveld123@gmail.com>
2025-07-28 14:55:37 +0200

    chore: hex and macroquad setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use hecs::World;
+use macroquad::prelude::*;
+
+// Components
+#[derive(Debug)]
+struct Position(Vec2);
+
+// Main
+#[macroquad::main("Collisions")]
+async fn main() {
+    let mut world = World::new();
+
+    world.spawn((Position(Vec2::ZERO),));
+
+    loop {
+        clear_background(BLACK);
+
+        render(&mut world);
+
+        next_frame().await
+    }
+}
+
+// Render system
+fn render(world: &mut World) {
+    for (_, pos) in world.query::<&Position>().iter() {
+        draw_rectangle(pos.0.x, pos.0.y, 16.0, 16.0, WHITE);
+    }
 }
