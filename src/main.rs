9c5617ee8d5404e13b02c215a6d44bb0c1ac4d1e
joeribreedveld <joeri.breedveld123@gmail.com>
2025-07-28 15:42:27 +0200

    feat: wall and collider setup

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ struct Position(Vec2);
 
 struct Player;
 
+struct Collider(Vec2);
+
 // Window config
 fn window_conf() -> Conf {
     Conf {
@@ -23,7 +25,16 @@ fn window_conf() -> Conf {
 async fn main() {
     let mut world = World::new();
 
-    world.spawn((Position(Vec2::ZERO), Player));
+    let screen_size = Vec2::new(screen_width(), screen_height());
+    let screen_center = screen_size / 2.0;
+
+    let entity_size = Vec2::new(DRAW_SIZE, DRAW_SIZE);
+
+    world.spawn((Position(screen_center), Player, Collider(entity_size)));
+
+    let wall_pos = Vec2::new(screen_center.x + 256.0, screen_center.y);
+
+    world.spawn((Position(wall_pos), Collider(entity_size)));
 
     loop {
         clear_background(BLACK);
@@ -51,14 +62,14 @@ fn movement(world: &mut World) {
         let mouse_pos = Vec2::from(mouse_position());
         let mouse_draw_pos = mouse_pos - DRAW_SIZE / 2.0;
 
-        draw_line(pos.0.x, pos.0.y, mouse_pos.x, mouse_pos.y, 2.0, GRAY);
+        draw_line(pos.0.x, pos.0.y, mouse_pos.x, mouse_pos.y, 2.0, DARKGRAY);
         draw_rectangle_lines(
             mouse_draw_pos.x,
             mouse_draw_pos.y,
             DRAW_SIZE,
             DRAW_SIZE,
             4.0,
-            GRAY,
+            DARKGRAY,
         );
 
         if is_mouse_button_pressed(MouseButton::Left) {
@@ -66,4 +77,9 @@ fn movement(world: &mut World) {
             pos.0.y = mouse_pos.y;
         }
     }
+}
+
+// Collision system
+fn collision(world: &mut World) {
+    for (_, (pos, collider)) in world.query_mut::<(&mut Position, &Collider)>() {}
 }
